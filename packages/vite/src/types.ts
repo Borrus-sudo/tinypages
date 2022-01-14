55630ed1f0e511d5057c3106c77ecb10c33a3a60
JSGandalf <jinmayjhaveri006@gmail.com>
2022-01-14 23:38:55 +0530

    style: add types for Meta

--- packages/vite/src/types.ts
+++ packages/vite/src/types.ts
@@ -1,11 +1,21 @@
 import { ViteDevServer } from "vite";
 
+type Meta = {
+  styles: string;
+  components: {
+    componentLiteral: string;
+    componentName: string;
+    props: string;
+    children: string;
+  }[];
+};
+
 type cascadeContext = {
   html: string;
-  meta: { components: string[]; styles: string };
+  meta: Meta;
   root: string;
   vite: ViteDevServer;
   compile: Function;
 };
 
-export { cascadeContext };
+export { cascadeContext, Meta };
