9e4c810e1b8888be4ef7de3913a0ef384ee1abc8
JSGandalf <jinmayjhaveri006@gmail.com>
2022-01-30 19:22:03 +0530

    feat(vite): optimize loading paths in a better async fashion

--- packages/vite/src/node/router/utils.ts
+++ packages/vite/src/node/router/utils.ts
@@ -1,15 +1,18 @@
 import { promises as fs } from "fs";
 import * as path from "path";
+
 export async function loadPaths(dir: string): Promise<string[]> {
   let result: string[] = [];
   const dirents = await fs.readdir(dir, { withFileTypes: true });
+  let folderLoadPaths = [];
   for (let dirent of dirents) {
     if (dirent.isDirectory()) {
-      result.push(...(await loadPaths(path.join(dir, dirent.name))));
+      folderLoadPaths.push(loadPaths(path.join(dir, dirent.name)));
     } else {
       result.push(path.join(dir, dirent.name));
     }
   }
-
+  const folderStructures = await Promise.all(folderLoadPaths);
+  result.push(...folderStructures.flat());
   return result;
 }
