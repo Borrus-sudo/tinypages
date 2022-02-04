642cd4ea1404c2d6404c3be290c0997532a00700
JSGandalf <jinmayjhaveri006@gmail.com>
2022-02-04 09:27:28 +0530

    feat: add component error boundary for csr components

--- packages/vite/src/client/entry-client.ts
+++ packages/vite/src/client/entry-client.ts
@@ -1,9 +1,9 @@
 import { h, render } from "preact";
-import hmr from "./hmr";
+import hmr, { toVNode, OLD_VNODE_FIELD } from "./hmr";
 
 declare const globals: Record<
   string,
-  { path: string; props: Record<string, string> }
+  { path: string; props: Record<string, string>; error: boolean }
 >;
 
 if (import.meta.env.DEV) {
@@ -16,23 +16,40 @@ export default async function () {
     let component = globals[uid];
     if (import.meta.env.DEV) {
       component.path = "/HIJACK_IMPORT" + component.path;
+      if (component.error) {
+        continue;
+      }
     }
-    const __comp__ = (await import(/* @vite-ignore */ component.path)).default;
-    let html =
-      element.getElementsByTagName("tinypages-fragment")?.[0]?.innerHTML;
-    const innerSlot = html
-      ? h("div", {
-          dangerouslySetInnerHTML: {
-            __html: html,
-          },
-        })
-      : null;
+    let html = "";
+    let innerSlot;
     const parent = element.parentElement;
-    const vnode = h(__comp__, component.props, innerSlot);
-    render(vnode, parent, element);
+    try {
+      const __comp__ = (await import(/* @vite-ignore */ component.path))
+        .default;
+      html = element.getElementsByTagName("tinypages-fragment")?.[0]?.innerHTML;
+      innerSlot = html
+        ? h("div", {
+            dangerouslySetInnerHTML: {
+              __html: html,
+            },
+          })
+        : null;
+      const vnode = h(__comp__, component.props, innerSlot);
+      render(vnode, parent, element);
+    } catch (err) {
+      if (import.meta.env.DEV) {
+        element.replaceWith(
+          `<div style="color:red; background-color: lightpink;border: 2px dotted black;margin-bottom: 36px;">${err}</div>`
+        );
+      }
+      console.error(err);
+    }
+
     if (import.meta.env.DEV) {
       element.setAttribute("preact", "");
       element.setAttribute("uid", uid);
+      //@ts-ignore
+      element[OLD_VNODE_FIELD] = toVNode(element);
     }
   }
 }
