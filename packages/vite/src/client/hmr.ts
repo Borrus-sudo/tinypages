8299a1a94c3175a680200048031f01a34d5f325a
JSGandalf <jinmayjhaveri006@gmail.com>
2022-02-04 09:28:03 +0530

    chore(vite): push to take help from @aids

--- packages/vite/src/client/hmr.ts
+++ packages/vite/src/client/hmr.ts
@@ -1,4 +1,41 @@
 import { refresh } from "million/refresh";
+import { OLD_VNODE_FIELD, m, DOMNode, VNode, Flags } from "million";
+export { toVNode, OLD_VNODE_FIELD } from "million";
+
+const myVnode = (el: DOMNode | string): VNode | undefined => {
+  if (typeof el === "string") {
+    const temp = document.createElement("div");
+    temp.innerHTML = el;
+    el = <DOMNode>temp.firstChild;
+  }
+
+  if (el[OLD_VNODE_FIELD]) return el[OLD_VNODE_FIELD];
+  if (el instanceof Text) return String(el.nodeValue);
+  if (el instanceof Comment) return undefined;
+
+  let isComponent = false;
+  const props = {};
+  // We know children length, so we created a fixed array
+  const children = new Array(el.children.length).fill(0);
+  for (let i = 0; i < el.attributes.length; i++) {
+    const { nodeName, nodeValue } = el.attributes[i];
+    if (nodeName === "preact") isComponent = true;
+    props[nodeName] = nodeValue;
+  }
+  for (let i = 0; i < el.childNodes.length; i++) {
+    children[i] = myVnode(<DOMNode>el.childNodes[i]);
+  }
+
+  console.log(el, isComponent);
+  const vnode = m(
+    el.tagName.toLowerCase(),
+    props,
+    children,
+    isComponent ? Flags.IGNORE_NODE : Flags.ANY_CHILDREN
+  );
+  el[OLD_VNODE_FIELD] = vnode;
+  return vnode;
+};
 
 export default function () {
   if (import.meta.hot) {
@@ -6,7 +43,8 @@ export default function () {
       import.meta.hot.invalidate();
     });
     import.meta.hot.on("new:document", (data: string) => {
-      refresh(data);
+      document.head.innerHTML = "";
+      refresh(data, myVnode);
     });
   }
 }
