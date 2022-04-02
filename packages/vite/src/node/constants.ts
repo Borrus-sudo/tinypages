0701afd97528693fdf3c245b43828529e6919e5d
(TS/JS).Gandalf(he/him) <58482194+Borrus-sudo@users.noreply.github.com>
2022-04-02 18:02:17 +0530

    chore: a fix

--- packages/vite/src/node/constants.ts
+++ packages/vite/src/node/constants.ts
@@ -11,7 +11,7 @@ export const presetCompilerConfig = {
   shiki: { themes: ["vitesse-dark", "nord"] },
   renderKatex: true,
   renderMermaid: false,
-  resolveUnoCSS: false,
+  renderUnoCSS: false,
   headTags: [
     `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css" integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs" crossorigin="anonymous">`,
   ],
