f0fee1790cbe1fb8fc33b371ca0507b496660245
(TS/JS).Gandalf(he/him) <58482194+Borrus-sudo@users.noreply.github.com>
2022-04-02 17:07:12 +0530

    chore: use esm ts-jest preset

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: "ts-jest",
+    "preset": "ts-jest/presets/default-esm",
+    "globals": {
+      "ts-jest": {
+        "useESM": true
+      }
+    },
     testEnvironment: "node",
     rootDir: "packages/",
     testMatch: ["<rootDir>/**/test/*.ts"],
