b3015259307652f97acc203e40fe3d12e1f2e0b9
JSGandalf <jinmayjhaveri006@gmail.com>
2022-04-08 22:55:43 +0530

    fix: fixing my shenanigans

--- packages/compiler/out/wrapObject.d.ts
+++
@@ -1,3 +0,0 @@
-declare function wrapObject(styles: Record<string, string>): Record<string, string>;
-
-export { wrapObject };
--- packages/compiler/out/wrapObject.mjs
+++
@@ -1,1 +0,0 @@
-function n(t){return Object.keys(t).forEach(e=>{t[e]=`"${t[e]}"`}),t}export{n as wrapObject};
