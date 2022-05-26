48aa98be6114fe53f49a867bcad35bb0d564550f
JSGandalf <jinmayjhaveri006@gmail.com>
2022-05-26 13:37:15 +0530

    fix(compiler): fix the test

--- packages/compiler/test/index.test.ts
+++ packages/compiler/test/index.test.ts
@@ -18,7 +18,6 @@ describe("it tests the functionality", () => {
         renderKatex: true,
         renderMermaid: true,
         renderUnoCSS: false,
-        renderTwemoji: false,
         headTags: [
           `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css" integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs" crossorigin="anonymous">`,
           `<link rel="stylesheet" href="index.css">`,
