914b54c34a601495a3e35bd178e9003b3e1702b0
JSGandalf <jinmayjhaveri006@gmail.com>
2022-06-17 23:32:43 +0530

    refactor(vite): refactor stuff

--- packages/vite/src/node/plugins/build/optimization(2).ts
+++ packages/vite/src/node/plugins/build/optimization(2).ts
@@ -3,7 +3,7 @@ import { useContext } from "../../context";
 
 /**
  * Build optimizations(2) for tinypages.
- * - The optimization is aimed to prevent cascading hash invalidation which can be a very prevalent problem for content based
+ * - The optimization is aimed to prevent cascading hash invalidation which is a very prevalent problem for content based
  * sites.
  * - The solution for this is yet to be decided. The first approach is with import maps (low support). The second one is with
  *   PWA but might hurt base JS bundle size more.
