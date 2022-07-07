8fc88520a36144b97d71477c7fff432b7cd9a434
JSGandalf <jinmayjhaveri006@gmail.com>
2022-07-07 18:37:25 +0530

    chore: add typings to playground and rename npm script

--- packages/playground/auto-imports.d.ts
+++ packages/playground/auto-imports.d.ts
@@ -10,3 +10,22 @@ declare global {
   const useRef: typeof import("preact/hooks")["useRef"];
   const useState: typeof import("preact/hooks")["useState"];
 }
+
+declare module "virtual:pwa-register/preact" {
+  import type { StateUpdater } from "preact/hooks";
+  export interface RegisterSWOptions {
+    immediate?: boolean;
+    onNeedRefresh?: () => void;
+    onOfflineReady?: () => void;
+    onRegistered?: (
+      registration: ServiceWorkerRegistration | undefined
+    ) => void;
+    onRegisterError?: (error: any) => void;
+  }
+
+  export function useRegisterSW(options?: RegisterSWOptions): {
+    needRefresh: [boolean, StateUpdater<boolean>];
+    offlineReady: [boolean, StateUpdater<boolean>];
+    updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
+  };
+}
